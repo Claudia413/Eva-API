767718fa1b1fcdbf53db39395c31523527085958
Claudia413 <cmjengelsman@gmail.com>
2017-06-28 12:04:27 +0200

    create new seeds for students and batches and seed

--- src/models/students.model.js
+++ src/models/students.model.js
@@ -2,26 +2,25 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
+module.exports = function(app) {
+  const mongooseClient = app.get("mongooseClient");
 
   const { Schema } = mongooseClient;
+
+  const batchSchema = new Schema({
+    number: { type: String, required: true },
+    startDate: { type: Date, default: Date.now },
+    endDate: { type: Date, default: Date.now }
+  });
 
   const students = new Schema({
     name: { type: String, required: true },
     picture: { type: String, required: true },
     grades: { type: Array },
-    batch: { type: Number},
+    batch: [batchSchema],
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
 
-  const batches = new Schema({
-    number: { type: String, required: true },
-    startDate: { type: Date, default: Date.now },
-    endDate: { type: Date, default: Date.now },
-    students: [students]
-  })
-
-  return mongooseClient.model('students', students);
+  return mongooseClient.model("students", students);
 };
